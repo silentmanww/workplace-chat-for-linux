f1c2e8806d2a49f711e03d311f3975553ea45da8
Erwan <erwan.benard@gmail.com>
2018-04-25 23:45:19 -0400

    cleaning 2

--- install.sh
+++ install.sh
@@ -20,16 +20,6 @@ npm install
 cd ${WORKDIR}/${APPNAME}
 npm install
 
-#if [[ $RELEASE =~ .*[A|a]"rch".* ]]; then
-#	sudo pacman -Ssy &>/dev/null
-#	sudo pacman -S wmctrl --needed --noconfirm &>/dev/null
-#elif [[ $RELEASE =~ .*[C|c]"ent"[O|o][S|s].* ]] || [[ $RELEASE =~ .*[R|r]"ed"[H|h]at.* ]] || [[ $RELEASE =~ .*[F|f]"edora".* ]]; then
-#	sudo yum -y install wmctrl &>/dev/null
-#elif [[ $RELEASE =~ .*[U|u]"buntu".* ]] || [[ $RELEASE =~ .*[D|d]"ebian".* ]]; then
-#	sudo apt update &>/dev/null
-#	sudo apt install -y wmctrl &>/dev/null
-#fi
-
 sudo cp -r ${WORKDIR}/${APPNAME} $INSTALLDIR
 sudo install -m 755 ${INSTALLDIR}/${APPNAME}/app/bin/${APPNAME} /usr/local/bin/
 install -m 755 ${INSTALLDIR}/${APPNAME}/app/bin/workplace-chat.desktop ~/.local/share/applications
