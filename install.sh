709508e484a0dd4f4fbfa9085b9da1dfb555a131
Erwan <erwan.benard@gmail.com>
2018-04-25 20:06:46 -0400

    Fix bin and desktop launcher

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm help
+npm help &>/dev/null
 if [ $? != 0 ]; then
 	echo "NPM need to be installed first, exiting..."
 	exit 1
@@ -12,6 +12,7 @@ cd ..
 npm install
 
 INSTALLDIR=/opt/workplacechat
-mkdir $INSTALLDIR
-install -m 755 * $INSTALLDIR
-install -m 755 lib/workplacechat ~/Desktop
+sudo mkdir -p $INSTALLDIR
+sudo cp -r * $INSTALLDIR
+sudo install -m 755 ${INSTALLDIR}/app/bin/workplace-chat /usr/local/bin/
+install -m 755 ${INSTALLDIR}/app/bin/workplacechat.desktop ~/Desktop
