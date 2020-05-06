0216dab39246e1e11aa9589059528bb2adf3cc6c
Angelo Theodorakis <silentmanww@gmail.com>
2020-05-06 15:06:36 +0100

    Notification counter in app status bar

--- app/lib/notifications.js
+++ app/lib/notifications.js
@@ -2,4 +2,23 @@ function(){
   const electron = require('electron');
   const remote = electron.remote;
   const NativeImage = electron.nativeImage;
+ 
+//  const { nativeImage } = require('electron');
+
+  exports = module.exports = ({ ipc, iconPath }) => {
+    return () => {
+      const icon = nativeImage.createFromPath(iconPath);
+      if (typeof Notify !== 'undefined') {
+        Notify.prototype.show = function show() {
+          const notification = new Notification(this.title, {
+            body: this.options.body,
+            icon: icon.toDataURL()
+          });
+          notification.onclick = () => {
+            ipc.send('nativeNotificationClick');
+          };
+        };
+      }
+    };
+  };  
 }
